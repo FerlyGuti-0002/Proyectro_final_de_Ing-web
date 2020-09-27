39aaa19e541f8ad140d4d9b4934da91a16f25b4a
FerlyGuti_0002 <ferlyquiroz17@gmail.com>
2020-09-26 21:25:22 -0600

    Enlace las páginas necesarias para apartar cada punto de la tienda, cree un boton llamado inicio en los Sidebar y ledi estilos de color a todas las páginas

--- Variedades-Ruby/Abrir.js
+++ Variedades-Ruby/Abrir.js
@@ -1,3 +1,5 @@
+/*Código para llamar a las clases utilizadas para poder concluir de forma correcta la funcionalidad 
+completa del Sidebar*/
 $('.menu-bar').on('click', function(){
     $('.Contenido').toggleClass('Abrir');
 })
